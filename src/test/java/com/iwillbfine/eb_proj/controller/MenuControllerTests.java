182a6d930476b6c4cb3a1c9a3f68188cd5b830ee
iwillbfine <iwillbfine@naver.com>
2024-11-05 13:54:15 +0900

    menu exists test

--- src/test/java/com/iwillbfine/eb_proj/controller/MenuControllerTests.java
+++ src/test/java/com/iwillbfine/eb_proj/controller/MenuControllerTests.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,6 +25,15 @@ class MenuControllerTests {
     public void healthCheckTest() throws Exception {
         mockMvc.perform(get("/health"))
                 .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @DisplayName("2번 메뉴 있는지 확인")
+    @Test
+    public void findMenuByMenuCodeTest() throws Exception {
+        mockMvc.perform(get("/menus/2"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.menuName").value("우럭스무디"))
                 .andDo(print());
     }
 }
