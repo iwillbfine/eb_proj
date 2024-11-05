83ab5b6b9f5e8d90ddfdfdebb44034901a63cbe0
iwillbfine <iwillbfine@naver.com>
2024-11-05 10:36:19 +0900

    findMenuByMenuCode()

--- src/main/java/com/iwillbfine/eb_proj/EbProjApplication.java
+++ src/main/java/com/iwillbfine/eb_proj/EbProjApplication.java
@@ -1,7 +1,10 @@
 package com.iwillbfine.eb_proj;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EbProjApplication {
@@ -10,4 +13,10 @@ public class EbProjApplication {
         SpringApplication.run(EbProjApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper;
+    }
 }
--- src/main/java/com/iwillbfine/eb_proj/controller/MenuController.java
+++ src/main/java/com/iwillbfine/eb_proj/controller/MenuController.java
@@ -1,13 +1,30 @@
 package com.iwillbfine.eb_proj.controller;
 
+import com.iwillbfine.eb_proj.dto.MenuDTO;
+import com.iwillbfine.eb_proj.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MenuController {
 
+    private final MenuService menuService;
+
+    @Autowired
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
     @GetMapping("health")
     public String health() {
         return "eb_proj is fine!";
+    }
+
+    @GetMapping("menus/{menuCode}")
+    public MenuDTO findMenuByMenuCode(@PathVariable("menuCode") int menuCode) {
+        MenuDTO returnMenu = menuService.findMenuByMenuCode(menuCode);
+        return returnMenu;
     }
 }
